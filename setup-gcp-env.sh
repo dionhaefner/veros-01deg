aae095c1889cc73db64e6c5f1792a1781bcf44dc
Dion Häfner <mail@dionhaefner.de>
2021-06-03 19:27:08 +0200

    Update setup-gcp-env.sh

--- setup-gcp-env.sh
+++ setup-gcp-env.sh
@@ -18,7 +18,7 @@ pip install numpy cython
 # install CUDA
 sudo wget -O /etc/apt/preferences.d/cuda-repository-pin-600 https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
 sudo apt-get install -y cuda
 echo 'export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}' >> ~/.bashrc
 echo 'export CUDA_ROOT=/usr/local/cuda' >> ~/.bashrc
